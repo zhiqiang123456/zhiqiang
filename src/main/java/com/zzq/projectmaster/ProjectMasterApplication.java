6796caefcca25917aa29ff25ed3e966dbb309bae
zzq123 <zhiqiang.zhang@gdsucang.com>
2025-01-26 13:58:12 +0800

    -

---
+++ src/main/java/com/zzq/projectmaster/ProjectMasterApplication.java
@@ -0,0 +1,13 @@
+package com.zzq.projectmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMasterApplication.class, args);
+    }
+
+}
